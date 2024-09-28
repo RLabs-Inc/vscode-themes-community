a3aa4ec72aacf4d16f1a39adcc2f0ea9330265e1
Rodrigo Luglio <rodrigo@rlabs.com.br>
2024-09-28 16:24:42 -0300

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
-import importMetaUrlPlugin from '@codingame/esbuild-import-meta-url-plugin'
 import { bundlerModuleNameResolver } from 'typescript'
 
 const nextConfig = {
   experimental: {
     turbo: {
-      // resolveAlias: {
-      //   'monaco-editor': 'monaco-editor/esm/vs/editor/editor.api',
-      // },
-
-      resolve: {
-        plugins: [bundlerModuleNameResolver],
+      resolveAlias: {
+        'monaco-editor': 'monaco-editor/esm/vs/editor/editor.api',
       },
     },
   },
