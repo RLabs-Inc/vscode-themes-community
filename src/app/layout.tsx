fcea122bd12bac2619ac8cd07e733069e35d1e4a
Rodrigo Luglio <rodrigo@rlabs.com.br>
2024-10-15 13:48:27 -0300

    disabled system theme for consistency on load with dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,6 @@ export default function RootLayout({
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
-            enableSystem
             disableTransitionOnChange
           >
             <ThemeProviderContext userId={userId ?? undefined}>
